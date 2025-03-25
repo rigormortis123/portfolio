e4b24454b90c9bb2e662c3df8082addf8aa91395
Marta <marta.venturacoelho@studentfr.ch>
2025-03-25 10:01:37 +0100

    smaccs mieux organiser, landing page terminer sauf le popup

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
-var resume = document.querySelector(".landing-popup");
+var resume = document.querySelector(".index-popup");
 var resumeButtom = document.getElementById("#resume");
+var overlay = document.querySelector(".l-overlay");
 
 var openResume = function () {
   resume.classList.toggle("is-open");
+  overlay.classList.toggle("is-active");
 };
 
 resumeButtom.addEventListener("click", openResume);
